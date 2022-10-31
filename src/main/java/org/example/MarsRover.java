433ccb04e7491a38801d3ff11297d4e37e3ec14b
David <ha_257@hotmail.com>
2022-10-31 14:51:19 +0100

    RED has_initial_starting_point_and_faces_north_direction

--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -1,11 +1,29 @@
 package org.example;
 
+import java.awt.*;
+
 public class MarsRover {
+    public MarsRover() {
+
+    }
+
+    public MarsRover(Point coordinates, String direction) {
+
+    }
+
     public void receives(String commands) {
 
     }
 
     public String getCommands() {
         return "FLFFRBLFR";
+    }
+
+    public Point getCoordinates() {
+        return null;
+    }
+
+    public String getDirection() {
+        return null;
     }
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -1,6 +1,8 @@
 import org.example.MarsRover;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MarsRoverTest {
@@ -13,5 +15,18 @@ public class MarsRoverTest {
         marsRover.receives(commands);
 
         assertEquals(commands, marsRover.getCommands());
+    }
+
+    @Test
+    void has_initial_starting_point_and_faces_north_direction() {
+        Point coordinates = new Point(0, 0);
+        String direction = "N";
+        MarsRover marsRover = new MarsRover(coordinates, direction);
+
+        Point marsRoverCoordinates = marsRover.getCoordinates();
+        String marsRoverDirection = marsRover.getDirection();
+
+        assertEquals(coordinates, marsRoverCoordinates);
+        assertEquals(direction, marsRoverDirection);
     }
 }
