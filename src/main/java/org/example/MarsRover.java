5f35cacdeb38ab1a5b58a379aa4c1f70b48c90ac
David MA <david.ma@lacombedulionvert.fr>
2022-11-08 15:07:07 +0100

    REFACTO use directions list and current direction index when testing direction

--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -1,13 +1,13 @@
 package org.example;
 
 import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class MarsRover {
     private String commands = "";
-
-    public MarsRover() {
-
-    }
+    private List<String> directions = Arrays.asList("N", "E", "S", "W");
+    private int currentDirectionIndex = 0;
 
     public MarsRover(Point coordinates, String direction) {
 
@@ -23,24 +23,30 @@ public class MarsRover {
 
     public Point getCoordinates() {
         if ("F".compareTo(this.commands) == 0) {
-            return new Point(1,2);
+            return new Point(1, 2);
         }
         if ("B".compareTo(this.commands) == 0) {
-            return new Point(1,1);
+            return new Point(1, 1);
         }
-        return new Point(1,1);
+        return new Point(1, 1);
     }
 
     public String getDirection() {
-        if ("L".compareTo(this.commands) == 0) {
-            return "W";
-        }
-        if ("R".compareTo(this.commands) == 0) {
-            return "E";
-        }
-        if ("RR".compareTo(this.commands) == 0) {
+        int lastDirectionIndex = directions.size() - 1;
+        boolean isTurnLeftCommand = "L".compareTo(this.commands) == 0;
+        boolean isTurnRightCommand = "R".compareTo(this.commands) == 0;
+
+        if (currentDirectionIndex == 0 && isTurnLeftCommand) {
+            currentDirectionIndex = lastDirectionIndex;
+        } else if (currentDirectionIndex == lastDirectionIndex && isTurnRightCommand) {
+            currentDirectionIndex = 0;
+        } else if (isTurnLeftCommand) {
+            currentDirectionIndex--;
+        } else if (isTurnRightCommand) {
+            currentDirectionIndex++;
+        } else if ("RR".compareTo(this.commands) == 0) {    // TODO if isTurnRightCommand x2, return S
             return "S";
         }
-        return "N";
+        return directions.get(currentDirectionIndex);       // TODO getDirection final implementation with reading from current direction index
     }
 }
